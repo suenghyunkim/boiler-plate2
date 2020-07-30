e7d0939a5de73fbada9ea038de7b73137dec355c
seunghyun kim <tmdgusdl92@gmail.com>
2020-07-31 07:32:31 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
 
+const config = require('./config/key')
+
+const {User} = require("./models/User");
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://johnahn:abcd1234@boilerplate-jyqvz.mongodb.net/<dbname>?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false}).then(()=>console.log('MongoDB Connected...')).catch(err=>console.log(err))
+mongoose.connect(config.mongoURI,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false}).then(()=>console.log('MongoDB Connected...')).catch(err=>console.log(err))
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post('/register',(req,res) =>{
+    const user = new User(req.body);
+
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
